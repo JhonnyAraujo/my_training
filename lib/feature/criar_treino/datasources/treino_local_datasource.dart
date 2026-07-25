a932cb7c86118cd9ea9d94df468a001f80e19969
JhonnyAraujo <jhonny.a.santana2002@gmail.com>
2026-07-24 23:34:30 -0300

    feat: adicionar tratamento de exceções no datasource

--- lib/feature/criar_treino/datasources/treino_local_datasource.dart
+++ lib/feature/criar_treino/datasources/treino_local_datasource.dart
@@ -7,24 +7,37 @@ class TreinoLocalDatasource {
   final SharedPreferencesAsync asyncPrefs = SharedPreferencesAsync();
 
   Future<void> salvarTreino({required List<TreinoModel> treinos}) async {
-    final jsonList = treinos.map((treino) => treino.toJson()).toList();
-
-    await asyncPrefs.setString('treinos', jsonEncode(jsonList));
+    try {
+      final jsonList = treinos.map((treino) => treino.toJson()).toList();
+      await asyncPrefs.setString('treinos', jsonEncode(jsonList));
+    } catch (_) {
+      throw Exception('Não foi possível salvar o treino');
+    }
   }
 
   Future<List<TreinoModel>> buscarTreinos() async {
-    final jsonString = await asyncPrefs.getString('treinos');
-    if (jsonString == null) return [];
+    try {
+      final jsonString = await asyncPrefs.getString('treinos');
+      if (jsonString == null) return [];
 
-    final decoded = jsonDecode(jsonString) as List;
-    return decoded.map((elemento) => TreinoModel.fromJson(elemento)).toList();
+      final decoded = jsonDecode(jsonString) as List;
+      return decoded.map((elemento) => TreinoModel.fromJson(elemento)).toList();
+    } catch (_) {
+      throw Exception('Não foi possível buscar o treino');
+    }
+    
   }
 
   Future<void> removeTreino({required int id}) async {
-    final treinos = await buscarTreinos();
+    try {
+      final treinos = await buscarTreinos();
 
-    treinos.removeWhere((treino) => treino.id == id);
+      treinos.removeWhere((treino) => treino.id == id);
 
-    await salvarTreino(treinos: treinos);
+      await salvarTreino(treinos: treinos);
+    } catch (_) {
+      throw Exception('Não foi possível remover o treino');
+    }
+    
   }
 } 
